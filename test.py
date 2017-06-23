a57e4e8e02968629db5159f9d690f7d4704ae286
Ryan Dorothy <ryan.dorothy@rackspace.com>
2017-06-23 14:52:04 -0500

    more exercises

--- test.py
+++ test.py
@@ -1,2 +1,37 @@
-msg = 'hello world'
-print msg
+
+
+# def fib_gen():
+#     a, b = 0, 1
+#     for i in xrange(0, 10):
+#         yield a
+#         (a, b) = (b, a + b)
+#
+# ry = fib_gen()
+# print ry.next()
+# print ry.next()
+# print ry.next()
+# print ry.next()
+# print ry.next()
+# print ry.next()
+
+sentence = "The words of a sentence!"
+def words_of_sentence(sentence):
+    punc = sentence[-1]
+    sentence = sentence.rstrip(punc)
+    words = sentence.split(' ')
+    return words, punc
+
+def put_back_together(words):
+    sentence = [' '.join(word) for word in words]
+    return sentence
+
+def attempt2(words, punctuation):
+    sent = ' '.join(words)
+    sentence = sent + punctuation
+    return sentence
+
+print words_of_sentence(sentence)
+print put_back_together(words_of_sentence(sentence))
+print "You failed, try again."
+values = words_of_sentence(sentence)
+print attempt2(*values)
