2f934b51711d862343247c7a7e224e9e16d719bd
Ryan Dorothy <ryan.dorothy@rackspace.com>
2017-06-28 14:42:17 -0500

    More complete exercises.

--- test.py
+++ test.py
@@ -14,34 +14,6 @@
 # print ry.next()
 # print ry.next()
 
-sentence = "The words of a sentence!"
-def words_of_sentence(sentence):
-    punc = sentence[-1]
-    sentence = sentence.rstrip(punc)
-    words = sentence.split(' ')
-    return words, punc
+d = {'name': 'ryan', 'age': 35, 'eyes': 'brown', 'hair': 'red'}
 
-def put_back_together(words):
-    sentence = [' '.join(word) for word in words]
-    return sentence
-
-def attempt2(words, punctuation):
-    sent = ' '.join(words)
-    sentence = sent + punctuation
-    return sentence
-
-print words_of_sentence(sentence)
-print put_back_together(words_of_sentence(sentence))
-print "Attempt2"
-values = words_of_sentence(sentence)
-print attempt2(*values)
-print values
-
-
-
-rally_tasks
-LICENSE
-README.rst
-disruptor.py
-requirements.txt	
-test_disruptor.py
+print d.items()
